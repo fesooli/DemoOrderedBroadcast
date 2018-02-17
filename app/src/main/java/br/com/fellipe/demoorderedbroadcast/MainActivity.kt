eb4cdbc6eca47abddf42c6448ab9396f2fff1840
fesooli <fellipe_so@hotmail.com>
2018-02-17 11:37:25 -0200

    alterações

--- app/src/main/java/br/com/fellipe/demoorderedbroadcast/MainActivity.kt
+++ app/src/main/java/br/com/fellipe/demoorderedbroadcast/MainActivity.kt
@@ -1,18 +1,33 @@
 package br.com.fellipe.demoorderedbroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var receiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnEnviar.setOnClickListener{
-            sendBroadcast(Intent("MeuOrderedBroadcast"))
+        var filter = IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when (intent?.action) {
+                    Intent.ACTION_POWER_CONNECTED -> tvUsbStatus.text = "Cabo Conectado"
+                    Intent.ACTION_POWER_DISCONNECTED -> tvUsbStatus.text = "Cabo Conectado"
+                }
+            }
         }
+        registerReceiver(receiver, filter)
     }
 }
